854ba74cc8b2ffd217c5af129094fab46e6e9df5
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-11-14 04:24:05 +0700

    Update crud_pymongo.py

--- crud_pymongo.py
+++ crud_pymongo.py
@@ -4,3 +4,11 @@ import pymongo
 client = pymongo.MongoClient("mongodb://localhost:0000/")
 db = client["ur_database"]
 collection = db["ur_collection"]
+
+def tambah_data():
+    nama = input("Masukkan nama produk: ")
+    harga = int(input("Harga: "))
+    negara = input("Dari Negara Mana: ")
+    data = {"nama": nama, "harga": harga, "negara":negara}
+    result = collection.insert_one(data)
+    print("Data berhasil ditambahkan dengan ID:", result.inserted_id)
