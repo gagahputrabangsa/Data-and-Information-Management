4783cc1d16bb68f87e5ea7b5ebf51a9efe8c530d
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-11-21 17:42:49 +0700

    Update and insert func

--- crud_menu.py
+++ crud_menu.py
@@ -4,6 +4,15 @@ import pymongo
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["prak8"]
 collection = db["MK"]
+
+def tambah_data():
+    nama = input("Masukkan nama produk: ")
+    harga = int(input("Harga: "))
+    negara = input("Dari Negara Mana: ")
+    data = {"nama": nama, "harga": harga, "negara":negara}
+    result = collection.insert_one(data)
+    print("Data berhasil ditambahkan dengan ID:", result.inserted_id)
+    
 def update_data():
     nama = input("Masukkan produk yang ingin diupdate: ")
     result = collection.find_one({"nama": nama})
