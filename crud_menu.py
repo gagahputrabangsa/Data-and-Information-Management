2ca50a0282acc11efd99de1703e552d4fe75f299
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-11-21 17:42:10 +0700

    insert func

--- crud_menu.py
+++ crud_menu.py
@@ -4,3 +4,13 @@ import pymongo
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["prak8"]
 collection = db["MK"]
+def update_data():
+    nama = input("Masukkan produk yang ingin diupdate: ")
+    result = collection.find_one({"nama": nama})
+    if result:
+        print("\nData ditemukan:")
+        print("Nama:", result['nama'])
+        print("Harga:", result['harga'])
+        print("Negara:", result['negara'])
+
+        pilihan = input("Apakah Anda ingin mengupdate: \n(1) Nama\n(2) Harga\n(3) Negara\nPilih (1/2/3): ")
