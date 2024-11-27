3dd76614bb2f7384f4ba36828e8bb188ea27b84d
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-11-27 22:44:18 +0700

    create node

--- py2neo.py
+++ py2neo.py
@@ -3,3 +3,11 @@ from py2neo import Graph
 # Conn
 graph = Graph("bolt://localhost:7687", auth=("neo4j", "ur_pass"))
 
+# Create
+graph.run("CREATE (p:Person {name: 'Reza', age: 18})")
+graph.run("CREATE (p:Person {name: 'Afni', age: 27})")
+graph.run("CREATE (p:Person {name: 'Agfan', age: 20})")
+graph.run("CREATE (p:Person {name: 'Arief', age: 25})")
+graph.run("CREATE (p:Person {name: 'Made', age: 31})")
+print("Node created!")
+
