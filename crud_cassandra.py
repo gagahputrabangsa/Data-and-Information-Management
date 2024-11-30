871410ba73c234cb6801f4e0f02cd4773491cfa0
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-11-30 11:24:43 +0700

    Update crud_cassandra.py

--- crud_cassandra.py
+++ crud_cassandra.py
@@ -1,4 +1,5 @@
 from cassandra.cluster import Cluster
+import uuid
 
 
 cluster = Cluster(['127.0.0.1']) 
@@ -11,3 +12,17 @@ WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'}
 """)
 
 session.set_keyspace('test_keyspace')
+# create table
+session.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY,
+    name TEXT,
+    email TEXT,
+    age INT
+)
+""")
+
+add data
+session.execute("""
+INSERT INTO users (id, name, email, age) VALUES (%s, %s, %s, %s)
+""", (uuid.uuid4(), 'John Doe', 'john@example.com', 30))
